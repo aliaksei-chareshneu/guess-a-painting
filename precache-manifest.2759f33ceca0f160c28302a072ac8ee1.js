e65ea16f6311cd23a4f5100cb1ee1b3f37227ebc
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2021-05-15 21:47:44 +0300

    Updates

---
+++ precache-manifest.2759f33ceca0f160c28302a072ac8ee1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661522b16f58d373433a905533914191",
+    "url": "/guess-a-painting/index.html"
+  },
+  {
+    "revision": "63f02b1f1cb39065c65d",
+    "url": "/guess-a-painting/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "73321d1715c8448cb6e9",
+    "url": "/guess-a-painting/static/css/main.bcdc82e6.chunk.css"
+  },
+  {
+    "revision": "63f02b1f1cb39065c65d",
+    "url": "/guess-a-painting/static/js/2.70b03e8c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/guess-a-painting/static/js/2.70b03e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73321d1715c8448cb6e9",
+    "url": "/guess-a-painting/static/js/main.9818f106.chunk.js"
+  },
+  {
+    "revision": "c62a4a7433f23e3af3fa",
+    "url": "/guess-a-painting/static/js/runtime-main.1a9a4cc5.js"
+  },
+  {
+    "revision": "50726969f0e293b990bc7aed356c668f",
+    "url": "/guess-a-painting/static/media/fareast.50726969.ttf"
+  }
+]);
